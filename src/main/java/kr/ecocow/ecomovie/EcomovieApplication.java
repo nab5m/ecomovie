ca13c85dd103a27960cf3124c6a6eeb0c5056015
JunYoung Kim <sk990420@naver.com>
2023-10-16 14:05:11 +0900

    Initial Commit

---
+++ src/main/java/kr/ecocow/ecomovie/EcomovieApplication.java
@@ -0,0 +1,13 @@
+package kr.ecocow.ecomovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ecocow/ecomovie/EcomovieApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ecocow.ecomovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
