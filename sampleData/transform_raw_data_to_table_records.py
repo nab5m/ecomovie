0cdcff251eb970336cc2a213a6d8c93a2b3450ec
JunYoung Kim <sk990420@naver.com>
2023-10-17 16:01:50 +0900

    insert code_item 샘플 데이터

--- sampleData/transform_raw_data_to_table_records.py
+++ sampleData/transform_raw_data_to_table_records.py
@@ -9,6 +9,7 @@ def write_dictionary_data_to_file(data, filename):
         file.write(json.dumps(language_list))
 
 status_set = set()
+status_dict = dict()
 genre_dict = dict()
 company_dict = dict()
 country_dict = dict()
@@ -22,11 +23,24 @@ for source_file_name in source_file_names:
 
         json_data = json.load(source_file)
         
-        status_set.add(json_data["status"])
+        status = json_data["status"]
+        count_before_add = len(status_dict)
+        status_set.add(status)
+
+        if len(status_set) > count_before_add:
+            status_dict[count_before_add + 1] = {
+                "code_category_id": 0,
+                "code_item_name": status,
+                "code": count_before_add
+            }
         
         genres = json_data["genres"]
         for genre in genres:
-            genre_dict[genre["id"]] = genre["name"]
+            genre_dict[genre["id"]] = {
+                "code_category_id": 0,
+                "code_item_name": genre["name"],
+                "code": genre["id"]
+            }
 
         production_companies = json_data["production_companies"]
         for production_company in production_companies:
@@ -46,10 +60,6 @@ for source_file_name in source_file_names:
             language["language_name"] = language["name"]
             del language["name"]
 
-print(status_set)
-print(genre_dict)
-print(company_dict)
-print(country_dict)
-print(collection_dict)
-
+write_dictionary_data_to_file(status_dict, "release_status.json")
+write_dictionary_data_to_file(genre_dict, "genres.json")
 write_dictionary_data_to_file(language_dict, "languages.json")
---
+++ src/main/resources/db/migration/mysql/V14__insert_code.sql
@@ -0,0 +1,40 @@
+SET @CODE_CATEGORY_CONTENT_TYPE_NAME = '컨텐츠종류';
+
+SELECT @CODE_CATEGORY_CONTENT_TYPE_ID := code_category_id FROM `code_category`
+    WHERE code_category_name = @CODE_CATEGORY_CONTENT_TYPE_NAME;
+
+INSERT INTO `code_item`(`code_category_id`, `code_item_name`, `code`) VALUES
+    (@CODE_CATEGORY_CONTENT_TYPE_ID, '영화', 0);
+
+SET @CODE_CATEGORY_GENRE_NAME = '장르';
+
+SELECT @CODE_CATEGORY_GENRE_ID := code_category_id FROM `code_category`
+    WHERE code_category_name = @CODE_CATEGORY_GENRE_NAME;
+
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Comedy',35);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Drama',18);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Science Fiction',878);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Crime',80);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Romance',10749);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Thriller',53);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Action',28);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Adventure',12);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Fantasy',14);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Horror',27);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Mystery',9648);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Animation',16);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Family',10751);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'War',10752);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Music',10402);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Western',37);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'History',36);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'TV Movie',10770);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_GENRE_ID,'Documentary',99);
+
+SET @CODE_CATEGORY_RELEASE_STATUS_NAME = '개봉상태';
+
+SELECT @CODE_CATEGORY_RELEASE_STATUS_ID := code_category_id FROM `code_category`
+    WHERE code_category_name = @CODE_CATEGORY_RELEASE_STATUS_NAME;
+
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_RELEASE_STATUS_ID,'Released',0);
+INSERT INTO code_item(code_category_id,code_item_name,code) VALUES (@CODE_CATEGORY_RELEASE_STATUS_ID,'Planned',1);
