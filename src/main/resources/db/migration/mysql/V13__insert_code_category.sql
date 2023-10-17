63370cd38a63831b2f15d7fa1e0d538654f2642a
JunYoung Kim <sk990420@naver.com>
2023-10-17 15:32:50 +0900

    code_category 테이블에 컨텐츠종류, 장르, 개봉상태 데이터 추가

---
+++ src/main/resources/db/migration/mysql/V13__insert_code_category.sql
@@ -0,0 +1,3 @@
+INSERT INTO `code_category`(`code_category_name`) VALUES ('컨텐츠종류');
+INSERT INTO `code_category`(`code_category_name`) VALUES ('장르');
+INSERT INTO `code_category`(`code_category_name`) VALUES ('개봉상태');
