d5fd3a57eb5c499dafc5104318a9c811b93769f1
JunYoung Kim <sk990420@naver.com>
2023-10-19 01:09:55 +0900

    SecurityConfig 추가: url 허용, cors 테스트

---
+++ src/main/java/kr/ecocow/ecomovie/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package kr.ecocow.ecomovie.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Value("${spring.h2.console.enabled}")
+    private boolean h2ConsoleEnabled;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+        if (h2ConsoleEnabled) {
+            // 프로덕션에서는 사용 x, h2-console 디버깅 시에만 써야됨
+            http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                authorizationManagerRequestMatcherRegistry.requestMatchers(toH2Console()).permitAll();
+            });
+            http.headers((httpSecurityHeadersConfigurer ->
+                    httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)));
+        }
+
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .mvcMatchers(HttpMethod.GET, "/docs/index.html").permitAll()
+                    .mvcMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
+                    .mvcMatchers(HttpMethod.GET, "/v3/api-docs").permitAll()
+                    .mvcMatchers(HttpMethod.GET, "/v3/api-docs/*").permitAll()
+                    .mvcMatchers(HttpMethod.GET, "/error").permitAll()
+                    .regexMatchers(HttpMethod.GET, "^/api/v1/movie/(\\d)+$").permitAll()
+                    .anyRequest().authenticated();
+        });
+
+        http.csrf().disable();
+        http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+
+        http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().mvcMatchers("/error");
+        };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("https://www.test-cors.org");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
