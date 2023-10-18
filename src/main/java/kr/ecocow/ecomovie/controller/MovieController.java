51a9d9db682a0a600f6dcce5bd21f34e26a0d945
JunYoung Kim <sk990420@naver.com>
2023-10-19 00:32:13 +0900

    [GET] /api/v1/movie/{movieId} 404 응답 추가, swagger 문서 구체화

--- src/main/java/kr/ecocow/ecomovie/controller/MovieController.java
+++ src/main/java/kr/ecocow/ecomovie/controller/MovieController.java
@@ -1,5 +1,8 @@
 package kr.ecocow.ecomovie.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.ecocow.ecomovie.controller.exception.NotFoundException;
+import kr.ecocow.ecomovie.controller.swagger.MovieControllerDocs;
 import kr.ecocow.ecomovie.dto.MovieDetailsDTO;
 import kr.ecocow.ecomovie.service.MovieService;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/movie")
 @RequiredArgsConstructor
+@Tag(name = "MovieController", description = "영화 컨트롤러")
 public class MovieController {
     private final MovieService movieService;
 
+    public static final String MOVIE_DETAILS_NOT_FOUND_MESSAGE = "존재하지 않는 영화입니다.";
+
     @GetMapping("{movieId}")
+    @MovieControllerDocs.GetMovieDetails
     public MovieDetailsDTO getMovieDetails(@PathVariable long movieId) {
-        return movieService.getMovieDetails(movieId);
+        MovieDetailsDTO movieDetailsDTO = movieService.getMovieDetails(movieId);
+
+        if (movieDetailsDTO == null) {
+            throw new NotFoundException(MOVIE_DETAILS_NOT_FOUND_MESSAGE);
+        }
+
+        return movieDetailsDTO;
     }
 }
---
+++ src/main/java/kr/ecocow/ecomovie/controller/swagger/MovieControllerDocs.java
@@ -0,0 +1,27 @@
+package kr.ecocow.ecomovie.controller.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import kr.ecocow.ecomovie.controller.MovieController;
+import kr.ecocow.ecomovie.dto.MovieDetailsDTO;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class MovieControllerDocs {
+    @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Operation(summary = "영화 상세 페이지 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "영화 상세 페이지 조회 성공",
+                    content = @Content(schema = @Schema(implementation = MovieDetailsDTO.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 영화",
+                    content = @Content(schema = @Schema(example = MovieController.MOVIE_DETAILS_NOT_FOUND_MESSAGE)))
+    })
+    public @interface GetMovieDetails { }
+}
