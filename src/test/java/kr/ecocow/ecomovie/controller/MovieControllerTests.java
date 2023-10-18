184ee297a1bc0117e89eec75bb9c31550f452055
JunYoung Kim <sk990420@naver.com>
2023-10-18 23:57:53 +0900

    영화 상세 페이지 정상 동작 확인하는 테스트 추가

---
+++ src/test/java/kr/ecocow/ecomovie/controller/MovieControllerTests.java
@@ -0,0 +1,27 @@
+package kr.ecocow.ecomovie.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MovieControllerTests {
+    @Autowired
+    private MovieController movieController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("영화 상세 페이지 정상 작동하는지 확인")
+    public void getMovieDetailsSuccess() throws Exception {
+        long movieId = 1L;
+        mockMvc.perform(get("/api/v1/movie/" + movieId));
+    }
+}
